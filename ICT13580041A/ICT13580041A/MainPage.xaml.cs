5bd64d77357b19e0310ca7c528df9b47dcd34ca2
Theera Khanthiyu <34564837+TheeraKhanthiyu@users.noreply.github.com>
2017-12-16 01:43:15 +0700

    init
    Project ICT13580041A
    (Page, Layout, View)

---
+++ ICT13580041A/ICT13580041A/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ICT13580041A
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            okButton.Clicked += OkButton_Clicked;
+            genderPicker.Items.Add("ชาย");
+            genderPicker.Items.Add("หญิง");
+
+            mySlider.ValueChanged += MySlider_ValueChanged;
+            myStepper.ValueChanged += MyStepper_ValueChanged;
+
+        }
+
+         void MyStepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valueLabel.Text = value.ToString();
+        }
+
+        void MySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valueLabel.Text = value.ToString();
+        }
+
+        void OkButton_Clicked(object sender, EventArgs e)
+        {
+            firstNameLabel.Text = firstNameEntry.Text;
+        }
+    }
+}
---
+++ ICT13580041A/ICT13580041A/NavPage3.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ICT13580041A
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavPage3 : ContentPage
+    {
+        public NavPage3()
+        {
+            InitializeComponent();
+            backButton.Clicked += BackButton_Clicked;
+            backToRootButton.Clicked += BackToRootButton_Clicked;
+        }
+
+        void BackToRootButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopToRootAsync();
+        }
+
+        void BackButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
